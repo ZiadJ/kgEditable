fc08ff7bae0f228bd914ed3114ca7296b1482a1b
ziadj <ziadjkhan@gmail.com>
2012-12-19 13:34:19 +0400

    Update kgTemplateSamples.js

--- kgTemplateSamples.js
+++ kgTemplateSamples.js
@@ -1,8 +1,3 @@
-// Observer plugin for Knockout http://knockoutjs.com/
-// (c) Ziad Jeeroburkhan
-// License: MIT (http://www.opensource.org/licenses/mit-license.php)
-// Version 0.2.2
-
 var tmlDefault = '<div><div data-bind="attr: { \'class\': \'kgCellText colt\' + $index()}, html: $data.getProperty($parent)" /></div></div>';
 
 var tplEdit = '<div><input type="text" data-bind="attr: { \'class\': \'kgCellInput colt\' + $index()}, value: $parent.entity[$data.field]" /></div>';
