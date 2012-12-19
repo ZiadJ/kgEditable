36c68bcf0ca91b1efd7e5ff6d395f9f1eb3ccd76
ziadj <ziadjkhan@gmail.com>
2012-12-19 13:58:25 +0400

    Update kgTemplateSamples.js

--- kgTemplateSamples.js
+++ kgTemplateSamples.js
@@ -1,8 +1,8 @@
-var tmlDefault = '<div><div data-bind="attr: { \'class\': \'kgCellText colt\' + $index()}, html: $data.getProperty($parent)" /></div></div>';
+var tplDefault = '<div><div data-bind="attr: { \'class\': \'kgCellText colt\' + $index()}, html: $data.getProperty($parent)" /></div></div>';
 
 var tplEdit = '<div><input type="text" data-bind="attr: { \'class\': \'kgCellInput colt\' + $index()}, value: $parent.entity[$data.field]" /></div>';
 
 function tmplDefaultEditable() {
-    return kgEditable(tmplDefault, 'tplEdit'); // The edit template needs to be referred to as a string for now.
+    return kgEditable(tplDefault, 'tplEdit'); // The edit template needs to be referred to as a string for now.
 }
 
