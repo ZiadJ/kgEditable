6ad6d4d1274424511425645ab5a1ba96c1fe31c6
ziadj <ziadjkhan@gmail.com>
2012-12-19 13:05:22 +0400

    Update kgEditable.js

--- kgEditable.js
+++ kgEditable.js
@@ -3,12 +3,12 @@
 // License: MIT (http://www.opensource.org/licenses/mit-license.php)
 // Version 0.2.2
 
-/* Sample editable templates with sample usage*/
+/* Sample editable templates */
 // var tplEdit = '<div><input type="text" data-bind="attr: { \'class\': \'kgCellInput colt\' + $index()}, value: $parent.entity[$data.field]" /></div>';
 
 // var tmlDefault = '<div><div data-bind="attr: { \'class\': \'kgCellText colt\' + $index()}, html: $data.getProperty($parent)" /></div></div>';
 
-// function tmplEditable() {
+// function tmplDefaultEditable() {
 //     return kgEditable(tmplDefault, 'tplEdit');
 // }
 
