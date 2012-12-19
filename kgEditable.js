ab3baf4961e471eab26e09b62afd3f1285909e0c
ziadj <ziadjkhan@gmail.com>
2012-12-19 13:08:05 +0400

    Update kgEditable.js

--- kgEditable.js
+++ kgEditable.js
@@ -10,7 +10,7 @@
 // var tplEdit = '<div><input type="text" data-bind="attr: { \'class\': \'kgCellInput colt\' + $index()}, value: $parent.entity[$data.field]" /></div>';
 
 // function tmplDefaultEditable() {
-//     return kgEditable(tmplDefault, 'tplEdit');
+//     return kgEditable(tmplDefault, 'tplEdit'); // The edit template needs to be referred to as a string for now.
 // }
 
 
