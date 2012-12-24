d757ad224cecdbbac0870e62520cad71dc03fdd7
ziadj <ziadjkhan@gmail.com>
2012-12-24 18:53:52 +0400

    Several bug fixes and new events added.
    Added the onEditStart and onEditEnd events and fixed several major bugs.

--- README.md
+++ README.md
@@ -9,19 +9,27 @@ Clicking on a cell will automatically put it into edit mode. However this can be
 <code>editOn</code> parameter. 
 
 ###Validation:###
-All edit events are directed to the 'kgOnChange' function(not customisable for now) on the view model which can be used 
-for validation or performing other actions.
+The following callback events are available on each cell:
+1. onEditStart
+2. onEditEnd
+3. onChange
+Those bring about enough control to help implement our own custom validation code.
 
 ###Change tracking:###
-Modified cells are automatically hilighted. To revert a cell back to its original value during editing just press the
+Modified cells are automatically hilighted. To revert a cell in edit state back to its original value just press the
 Esc key.
 
 ###Navigation:###
-Keyboard navigation is currently enabled using the  Enter|Tab, shift > Enter|Tab, Up and Down keys.
+Keyboard navigation is currently enabled using the Enter|Tab, shift > Enter|Tab, Up and Down keys while the left and right keys
+control the cursor position inside the input elements. This however may change in later versions depending on user feedback.
+
+###Styling:###
+The edit template is always appended to the root element of the readonly template. This allows it to inherit any css
+styling such as font color that is applied to the latter.
 
 ###Performance:###
-No edit markup is loaded when the grid is instanciated. It only happens when a cell is edited. Therefore the markup
-used in the the edit template should not affect the overall grid performance.
+The edit template is only dynamically loaded whenever a cell is edited. Therefore the template content should not 
+affect the overall grid performance.
 
 ###Usage:###
 <code>
@@ -35,14 +43,16 @@ var tmplEdit = '<...>';
 <br/>
 <code>
 var columnDefinitions = ko.observableArray([<br/>
-&nbsp;&nbsp;&nbsp;&nbsp;{ field: 'SomeFieldName', cellTemplate: kgEditable(tplDefault, 'tplEdit', 'dblclick', 'orange', 'onModified') },<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;{ field: 'SomeFieldName', cellTemplate: kgEditable(tplDefault, 'tplEdit', 'dblclick', 'orange', 'onModified') } <br/>
 ]);
 </code>
 
 ###Demo:###
 Here's a basic demo:
-http://jsfiddle.net/K5wZ6
-P.S This might not work in IE due to some jsFiddle issues.
+http://jsfiddle.net/eV5Ly <br/>
+P.S This may not work in IE due to some jsFiddle issues.
+
+
 
 I'm commited to develop this project further depending on public interest. So you're most welcome to post suggestions 
 and/or contribute. 
--- kgEditable.js
+++ kgEditable.js
@@ -1,78 +1,77 @@
 // Observer plugin for Knockout http://knockoutjs.com/
 // (c) Ziad Jeeroburkhan
 // License: MIT (http://www.opensource.org/licenses/mit-license.php)
-// Version 0.5.0 beta
+// Version 0.7.5 beta
 
-function kgEditable(cellTemplate, editCellTemplateName, editOn, hilightClass, onChangeCallbackName) {
+function kgEditable(cellTemplate, editCellTemplateName, editOn, hilightClass, onEditStartCallbackName, onChangeCallbackName, onEditEndCallbackName) {
     var tpl = ko.utils.unwrapObservable(cellTemplate);
     tpl = $(cellTemplate).addClass('kgEditable').attr('data-bind', ', kgEditable: { '
         + (!editCellTemplateName ? '' : 'editTemplateName: ' + editCellTemplateName)
         + (!editOn ? '' : ', editOn: "' + editOn + '"')
         + (!hilightClass ? '' : ', hilightClass: "' + hilightClass + '"')
         + (!onChangeCallbackName ? '' : ', onChange: "' + onChangeCallbackName + '"')
+        + (!onEditStartCallbackName ? '' : ', onEditStart: "' + onEditStartCallbackName + '"')
+        + (!onEditEndCallbackName ? '' : ', onEditEnd: "' + onEditEndCallbackName + '"')
         + ' }').wrap('<a>').parent().html();
     return tpl;
 }
 
 ko.bindingHandlers.kgEditable = {
-    init: function (element, accessor) {
+    init: function (element, accessor, allBindings, viewModel, ctx) {
         var options = ko.utils.unwrapObservable(accessor());
+        var val = ctx.$parent.entity[ctx.$data.field];
+        if (val.initValue && val.initValue != ko.utils.unwrapObservable(val))
+            hilightCell($(element), true, options.hilightClass);
 
-        $(element).bind(options.editOn || 'click', function () {
-            $(element).trigger('edit');
-        }).bind('edit', function () {
-            var ctx = ko.contextFor(element);
-            var val = ctx.$parent.entity[ctx.$data.field];
+        $(element).on(options.editOn || 'click', function () {
+            $(this).trigger('edit');
+        }).on('edit', function () {
             var $elem = $(element);
-			
             $elem.children(':not(.kgEditCell)').hide();
 
-            if (val.initValue === undefined) {
+            if (!$elem.children('.kgEditCell').length) {
                 val.initValue = ko.utils.unwrapObservable(val);
 
                 var editElement = $(options.editTemplateName || options).addClass('kgEditCell');
                 $elem.append(editElement);
 
-                twoWayBoundHtml = $elem.html().replace(/\$data.getProperty\(\$parent\)/g, '$parent.entity[$data.field]')
-                                              .replace(/getProperty\(\$parent\)/g, '$parent.entity[$data.field]');
-                $elem.html(twoWayBoundHtml);
+                twoWayBoundElements = $elem.html().replace(/\$data.getProperty\(\$parent\)/g, '$parent.entity[$data.field]')
+                                                  .replace(/getProperty\(\$parent\)/g, '$parent.entity[$data.field]');
+                $elem.html(twoWayBoundElements);
 
+                ko.cleanNode(element);
                 ko.applyBindings(ctx, element);
 
-                findInputElements().blur(function () {
+                findInputElements($elem).blur(function () {
                     setTimeout(function () {
-                        if (findInputElements().is(":focus"))
+                        if (findInputElements($elem).is(":focus"))
                             return;
 
-                        $elem.children('.kgEditCell').hide();
+                        $elem.children('.kgEditCell').hide();//.remove();
                         $elem.children(':not(.kgEditCell)').show();
 
                         var hasChange = val.initValue != ko.utils.unwrapObservable(val);
 
-                        if (!options.hilightClass)  // || options.hilightClass.match(/rgb\(|rgba\(|\#[0-9]?/gi))
-                            $elem.css('backgroundColor', !hasChange ? '' : options.hilightClass || 'rgba(255, 95, 0, 0.3)');
-                        else if (hasChange)
-                            $elem.addClass(options.hilightClass);
-                        else
-                            $elem.removeClass(options.hilightClass)
+                        hilightCell($elem, hasChange, options.hilightClass);
 
-                        if (hasChange && ctx.$parent.$userViewModel.kgOnChange)
-                            ctx.$parent.$userViewModel[options.onChange || 'kgOnChange'].call(ctx, ctx.$data.field,
-                                ko.utils.unwrapObservable(val), val.initValue, element);
+                        if (hasChange && options.onChange)
+                            ctx.$parent.$userViewModel[options.onChange].call(ctx, val, element);
+
+                        if (options.onEditEnd)
+                            ctx.$parent.$userViewModel[options.onEditEnd].call(ctx, val, element);
                     }, 0);
                 });
 
                 $elem.keydown(function (e) {
-                    if ([13, 9, 38, 40].indexOf(e.which) >= 0)
-                        return false;
+                    return [13, 9, 38, 40].indexOf(e.which) < 0;
                 }).keyup(function (e) {
                     switch (e.which) {
                         case 27:
+                            findInputElements($elem).blur();
                             ko.applyBindings(ctx, element);
-                            findInputElements().blur();
                             break;
                         case 13:
-                            //findInputElements().blur();
+                            //findInputElements($elem).blur();
                             //break;
                         case 9:
                             var cell = $elem.parents('.kgCell').first();
@@ -105,19 +104,31 @@ ko.bindingHandlers.kgEditable = {
             }
 
             $elem.children('.kgEditCell').show();
-            var inputElements = findInputElements();
+            var inputElements = findInputElements($elem);
             var hasFocusedInput = false;
             $.each(inputElements, function () { if ($(this).is(":focus")) hasFocusedInput = true; });
             if (!hasFocusedInput)
                 inputElements.first().select().focus();
 
-            function findInputElements() {
-                var editElem = $elem.children('.kgEditCell');
-                var inputElems = editElem.find('.kgInput');
-                if (inputElems.length)
-                    return inputElems;
-                return editElem.find('input').last();
-            }
+            if (options.onEditStart)
+                ctx.$parent.$userViewModel[options.onEditStart].call(ctx, val, element);
         });
+
+        function findInputElements($element) {
+            var editElem = $element.children('.kgEditCell');
+            var inputElems = editElem.find('input.kgInput');
+            if (inputElems.length)
+                return inputElems;
+            return editElem.find('input');
+        }
+
+        function hilightCell($element, hasChange, hilightClass) {
+            if (!options.hilightClass)  // || options.hilightClass.match(/rgb\(|rgba\(|\#[0-9]?/gi))
+                $element.css('backgroundColor', !hasChange ? '' : hilightClass || 'rgba(255, 95, 0, 0.3)');
+            else if (hasChange)
+                $element.addClass(hilightClass);
+            else
+                $element.removeClass(hilightClass)
+        }
     }
 };
